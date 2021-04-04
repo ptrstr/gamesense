35b63b0919ba03c358064cfee6abb02c299f0e1c
ptrstr <ptrstr@protonmail.com>
2021-04-04 18:16:40 -0400

    Add basic documentation and README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gamesense
-GameSense™ client written in Rust
+
+[![crates.io](https://img.shields.io/crates/v/gamesense.svg)](https://crates.io/crates/gamesense)
+[![Documentation](https://docs.rs/gamesense/badge.svg)](https://docs.rs/gamesense)
+[![MIT | Apache-2 licensed](https://img.shields.io/crates/l/gamesense.svg)](./LICENSE-APACHE)
+
+SteelSeries GameSense™ client written in Rust
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! SteelSeries GameSense™ client written in Rust
+//!
+//!
+//! To use this crate, simply add it as a dependency:
+//! ```toml
+//! [dependency]
+//! gamesense = "0.1.0"
+//! ```
+//!
+//!
+//! To get started, simply instantiate your client using the [`gamesense::client::GameSenseClient`][client::GameSenseClient] struct.
+//! ```
+//! let mut client = GameSenseClient::new("GAME_ID", "Game Display Name", "Author", None)?;
+//! ```
+//!
+//! In this example, a client is created to the local API and it automatically creates a game with the provided values. Each value
+//! deemed optional by the official documentation can receive `None` as a value to default it to the server's default.
+//! If you wish to have a raw client for the API, you can! Simply use the [`gamesense::raw_client::RawGameSenseClient`][raw_client::RawGameSenseClient]
+//!
+//! For more (in-depth) examples refer to the [examples](https://github.com/ptrstr/gamesense/examples)
+//!
+//! For information regarding the API see the [original API documentation](https://github.com/SteelSeries/gamesense-sdk)
+
 extern crate anyhow;
 extern crate reqwest;
 extern crate serde;
